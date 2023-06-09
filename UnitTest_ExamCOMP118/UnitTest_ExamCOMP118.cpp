675e528ad70343d82df75acdbb3f1c5c3a5874a9
Pepega2005 <elite.clone69@gmail.com>
2023-06-09 13:02:25 +0300

    Created a Test Project and writing a Unit Test

---
+++ UnitTest_ExamCOMP118/UnitTest_ExamCOMP118.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Exam_COMP118/Exam_COMP118.cpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestExamCOMP118
+{
+	TEST_CLASS(UnitTestExamCOMP118)
+	{
+	public:
+		
+		TEST_METHOD(TestMethodSumOfOnes)
+		{
+			const double sum = 2;
+			const double matrix[MAX_ROW][MAX_COL] = {1, 0, 0, 1};
+
+			SumOfOnes
+		}
+	};
+}
