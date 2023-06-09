7ecfd6d5424075f50698e49fba686e96b544aabe
Student <Student@B101-04>
2023-06-09 12:11:40 +0300

    Add project files.

---
+++ Exam_COMP118/Exam_COMP118.cpp
@@ -0,0 +1,24 @@
+/** \file Exam_COMP118.cpp
+* \brief 
+* \details
+* \author Igor Kuvshinov
+* \date 09.06.2023
+* \version v0.1
+* */
+
+#include <iostream>
+
+using namespace std;
+
+//Function prototypes
+
+/** <code>main</code>Main function
+* <BR>
+* Return Returns 0
+*/
+int main()
+{
+    cout << "Hello World!\n";
+
+    return 0;
+}
