85a58ce09dbf33aafeba8bc48c7ad774dcbe3c89
Pepega2005 <elite.clone69@gmail.com>
2023-06-09 12:29:15 +0300

    Making the first function EnterData

--- Exam_COMP118/Exam_COMP118.cpp
+++ Exam_COMP118/Exam_COMP118.cpp
@@ -7,6 +7,7 @@
 * */
 
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
@@ -15,8 +16,9 @@ const int MAX_ROW = 4;
 const int MAX_COL = 3;
 
 //Function prototypes
+void EnterData(double matrix[][MAX_COL], int rowMax);
 
-/** <code>main</code>Main function
+/** <code>main</code>Main function with a menu
 * <BR>
 * Return Returns 0
 */
@@ -38,12 +40,42 @@ int main()
         {
         case 1:
 
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 4:
+            break;
         default:
             cout << "Invalid input!" << endl;
             break;
         }
+
+        cout << endl;
     } while (choice != 4);
     
+    cout << "Have a nice day!" << endl;
 
     return 0;
+}
+
+/** <code>EnterData</code> Function that asks a user to
+* input values into a matrix
+* <BR>
+* @param matrix Array of numbers
+*/
+void EnterData(double matrix[][MAX_COL], const int rowMax)
+{
+    assert(rowMax > 0 && rowMax <= MAX_ROW);
+
+    for (int i = 0; i < rowMax; i++)
+    {
+        for (int j = 0; j < MAX_COL; j++)
+        {
+
+        }
+    }
 }
