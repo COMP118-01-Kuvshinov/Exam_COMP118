ae6ecfccd82e8b5d6192efb0dfd9e4e9ce8fd0e9
Pepega2005 <elite.clone69@gmail.com>
2023-06-09 12:19:46 +0300

    Created a menu

--- Exam_COMP118/Exam_COMP118.cpp
+++ Exam_COMP118/Exam_COMP118.cpp
@@ -1,6 +1,6 @@
 /** \file Exam_COMP118.cpp
-* \brief 
-* \details
+* \brief Working with a matrix
+* \details Program that is manipulating with a matrix of doubles
 * \author Igor Kuvshinov
 * \date 09.06.2023
 * \version v0.1
@@ -10,6 +10,10 @@
 
 using namespace std;
 
+// Constants
+const int MAX_ROW = 4;
+const int MAX_COL = 3;
+
 //Function prototypes
 
 /** <code>main</code>Main function
@@ -18,7 +22,28 @@ using namespace std;
 */
 int main()
 {
-    cout << "Hello World!\n";
+    double matrix[MAX_ROW][MAX_COL];
+    int choice = 0;
+
+    do
+    {
+        cout << "1) Enter data in matrix (range -2.5 to 2.5)" << endl
+            << "2) Sum of all numbers" << endl
+            << "3) Show data in matrix" << endl
+            << "4) Exit" << endl
+            << "Please enter your choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+
+        default:
+            cout << "Invalid input!" << endl;
+            break;
+        }
+    } while (choice != 4);
+    
 
     return 0;
 }
